96776b1523544d738ed98526981cba6c14d6ba6a
Waldemar Bira <waldek.bira@gmail.com>
2017-02-15 20:00:52 +0100

    Displayed todos

--- src/Tasks.js
+++ src/Tasks.js
@@ -1,9 +1,22 @@
 import React from 'react'
 
 export default React.createClass({
+  getInitialState () {
+    return {
+      todos: [
+        { id: 1, text: 'Task 1', completed: false },
+        { id: 2, text: 'Task 2', completed: false },
+        { id: 3, text: 'Task 3', completed: false }
+      ]
+    }
+  },
+
   render () {
     return (
-      <h1>Todos</h1>
+      <div>
+        <h1>Todos</h1>
+        {this.state.todos.map(todo => todo.text)}
+      </div>
     )
   }
 })
